1e9bb521da3832bd3277ec59745e01dc1007137d
Bohdan <example@example>
2020-07-31 23:25:02 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+
+let numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?", " ");
+
+let personalMowieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+ const a = prompt("Один из последних просмотреных фильмов?", " "),
+       b = prompt("На сколько оцените его?", " "),
+       c = prompt("Один из последних просмотреных фильмов?", " "),
+       d = prompt("На сколько оцените его?", " ");
+
+personalMowieDB.movies[a] = [b];
+personalMowieDB.movies[c] = [d];
+
+console.log(personalMowieDB);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
