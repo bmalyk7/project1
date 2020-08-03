a749fc279458fa28f667d630f53ea3e9a9cd52e9
Bohdan <example@example>
2020-08-03 12:10:54 +0200

    second practice

--- js/script.js
+++ js/script.js
@@ -11,30 +11,63 @@ const personalMowieDB = {
     privat: false
 };
 
- const a = prompt("Один из последних просмотреных фильмов?", " "),
-       b = prompt("На сколько оцените его?", " "),
-       c = prompt("Один из последних просмотреных фильмов?", " "),
-       d = prompt("На сколько оцените его?", " ");
+/*for(let i = 0; i<2; i++){
+    const a = prompt("Последний просмотренный фильм?", " "),
+          b = prompt("Как оцените?", " ");
+    if(a != null && b !=null && a!=0 && b!=0 && a.length < 50) {
+        personalMovieDB.movies [a] = b;
+        console.log("DOne");
+        
+    }      else {
+        i--;
+    }
+    
+}
 
-personalMowieDB.movies[a] = [b];
-personalMowieDB.movies[c] = [d];
+console.log(personalMowieDB);*/
+
+
+/*let i = 0;
+
+
+do {
+    let a = prompt("Последний просмотренный фильм?", " "),
+        b = prompt("Как оцените?", " ");
+        i++;
+    if(a !=null && b !=null && a!=0 && b!=0 && a.length < 50){
+        personalMowieDB.movies[a] = b;
+        console.log("done");
+    } else { 
+        i--;
+    }
+
+    
+} while(i < 2);*/
+
+
+let i = 0;
+
+while(i < 2) {
+    let a = prompt("Последний просмотренный фильм", " "),
+        b = prompt("Как оцените?", " ");
+        i++;
+ if( a != null && b != null && a !=0 && b != 0 && a.length < 50) {
+     personalMowieDB.movies[a] = b;
+     console.log("Done");
+} else {
+    i--;
+}
+
+}
+
+
+if(numberOfFilms <= 10) {
+    alert(" Очень мало фильмов!");
+} else if(numberOfFilms >=10 && numberOfFilms <=30) {
+    alert("Вы классический киноман");
+} else {
+    alert(" Воу!");
+}
 
 console.log(personalMowieDB);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
